ae5e8a8b2399e5a24cc1e38bc4f5f22d84fed4ab
Freya <freyamurphy@gmail.com>
2019-01-22 16:54:54 +0000

    Made a HashMap to store the staff details and tried to make it work with JSP. Still trying to figure that bit out.

--- src/java/staff/StaffHandler.java
+++ src/java/staff/StaffHandler.java
@@ -8,13 +8,39 @@ package staff;
  *
  * @author freyamurphy
  */
-//import Java.util.HashMap;
+import java.util.HashMap;
 
 public class StaffHandler {
     private boolean empty;
-    //private staffList;
+    private HashMap<String, Staff> staffList;
     
     public StaffHandler() {
         empty = true;
-}
+        staffList = new HashMap<String, Staff>();
+        Staff staff1 = new Staff("Freya", "123");
+        Staff staff2 = new Staff("Momoko", "abc");
+        Staff staff3 = new Staff("Shanshan", "hello");
+        addStaff(staff1);
+        addStaff(staff2);
+        addStaff(staff3);
+    }
+    
+    public boolean isEmpty() {
+        return empty;
+    }
+    
+    public void addStaff(Staff newMember) {
+        String username = newMember.getUsername();
+        staffList.put(username, newMember);
+    }
+    
+    public Staff getStaffMember(String username) {
+        Staff foundMember = staffList.get(username);
+        return foundMember;
+    }
+    
+    public String getPassword(String username) {
+        Staff foundMember = staffList.get(username);
+        return foundMember.getPassword();
+    }
 }
