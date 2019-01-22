38e8269c5d515ef6551b3ee8d57ff8ccc52f6203
Freya <freyamurphy@gmail.com>
2019-01-22 12:14:39 +0000

    Started login user story. Made Staff classes and an html AdminLogIn page. Not much functionality, just lots of new files.

---
+++ src/java/staff/StaffHandler.java
@@ -0,0 +1,20 @@
+/*
+ * A class to store all the users of the system.
+ * Maybe this should be done a different way or called something else.
+ */
+package staff;
+
+/**
+ *
+ * @author freyamurphy
+ */
+import Java.util.HashMap;
+
+public class StaffHandler {
+    private boolean empty;
+    private staffList
+    
+    public StaffHandler() {
+        empty = true;
+}
+}
