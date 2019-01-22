ee35c04eb0475417d84141004147a157cc2d8675
momoko-w <23170782+momoko-w@users.noreply.github.com>
2019-01-22 15:41:14 +0000

    Create exam functionatility + tests added

---
+++ src/java/classes/ExamStorage.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+import java.util.*;
+
+/**
+ *
+ * @author Momoko Werner
+ */
+public class ExamStorage {
+    //
+    //HashMap<String, Exam> examList;
+    //constructor 
+    public ExamStorage()
+    {
+        //examList = new HashMap<>();
+        //add functionality to read in list of stored exams
+        
+    }
+    
+    //method to add an exam to the exam storage
+    public void addExamToList(Exam newExam)
+    {
+        //
+        
+    }
+}
