b0e5d5cad094e4e12e7821214eb0b14fbfff616e
Freya <freyamurphy@gmail.com>
2019-01-23 11:06:37 +0000

    Finished user login including tests

--- src/java/staff/StaffHandler.java
+++ src/java/staff/StaffHandler.java
@@ -11,36 +11,125 @@ package staff;
 import java.util.HashMap;
 
 public class StaffHandler {
-    private boolean empty;
-    private HashMap<String, Staff> staffList;
+    private HashMap<String, Admin> adminList;
+    private HashMap<String, InternalModerator> iMList;
+    private HashMap<String, ExamSetter> eSList;
     
     public StaffHandler() {
-        empty = true;
-        staffList = new HashMap<String, Staff>();
-        Staff staff1 = new Staff("Freya", "123");
-        Staff staff2 = new Staff("Momoko", "abc");
-        Staff staff3 = new Staff("Shanshan", "hello");
-        addStaff(staff1);
-        addStaff(staff2);
-        addStaff(staff3);
+        adminList = new HashMap<String, Admin>();
+        iMList = new HashMap<String, InternalModerator>();
+        eSList = new HashMap<String, ExamSetter>();
+        
+        addExampleStaff();
     }
     
-    public boolean isEmpty() {
-        return empty;
+    public void addExampleStaff() {
+        Admin craig = new Admin("Craig", "craig");
+        InternalModerator bob = new InternalModerator("Bob", "123");
+        InternalModerator guy = new InternalModerator("Guy", "abc");
+        ExamSetter lily = new ExamSetter("Lily", "asdf");
+        ExamSetter bob2 = new ExamSetter("Bob", "123456");
+        Boolean bla = addAdmin(craig);
+        Boolean bla2 = addInternalModerator(bob);
+        Boolean bla3 = addInternalModerator(guy);
+        Boolean bla4 = addExamSetter(lily);
+        Boolean bla5 = addExamSetter(bob2);
     }
     
-    public void addStaff(Staff newMember) {
+    public boolean addAdmin(Admin newMember) {
         String username = newMember.getUsername();
-        staffList.put(username, newMember);
+        
+        // Username is unique
+        if (getAdmin(username) == null) {
+            adminList.put(username, newMember);
+            return true;
+        }
+        
+        // This is a bad implementation because if the username is already taken
+        // no feedback is sent to the user to tell them the new admin has not been added.
+        // I tried to throw an exception if the username was taken but Java didn't like it
+        // because the exception wasn't caught anywhere.
+        return false;
+        
     }
     
-    public Staff getStaffMember(String username) {
-        Staff foundMember = staffList.get(username);
-        return foundMember;
+    public boolean addInternalModerator(InternalModerator newMember) {
+        String username = newMember.getUsername();
+        
+        // Username is unique
+        if (getIM(username) == null) {
+            iMList.put(username, newMember);
+            return true;
+        }
+        
+        // This is a bad implementation because if the username is already taken
+        // no feedback is sent to the user to tell them the new IM has not been added.
+        // I tried to throw an exception if the username was taken but Java didn't like it
+        // because the exception wasn't caught anywhere.]
+        return false;
     }
     
-    public String getPassword(String username) {
-        Staff foundMember = staffList.get(username);
+    public boolean addExamSetter(ExamSetter newMember) {
+        String username = newMember.getUsername();
+        
+        // Username is unique
+        if (getES(username) == null) {
+            eSList.put(username, newMember);
+            return true;
+        }
+        
+        // This is a bad implementation because if the username is already taken
+        // no feedback is sent to the user to tell them the new ES has not been added.
+        // I tried to throw an exception if the username was taken but Java didn't like it
+        // because the exception wasn't caught anywhere.
+        return false;
+    }
+    
+    public Admin getAdmin(String username) {
+        Admin foundAdmin = adminList.get(username);
+        if (foundAdmin == null) {
+            return null;
+        }
+        return foundAdmin;
+    }
+    
+    public InternalModerator getIM(String username) {
+        InternalModerator foundIM = iMList.get(username);
+        if (foundIM == null) {
+            return null;
+        }
+        return foundIM;
+    }
+    
+    public ExamSetter getES(String username) {
+        ExamSetter foundES = eSList.get(username);
+        if (foundES == null) {
+            return null;
+        }
+        return foundES;
+    }
+    
+    public String getAdminPassword(String username) {
+        Admin foundMember = adminList.get(username);
+        if (foundMember == null) {
+            return null;
+        }
+        return foundMember.getPassword();
+    }
+    
+    public String getIMPassword(String username) {
+        InternalModerator foundMember = iMList.get(username);
+        if (foundMember == null) {
+            return null;
+        }
+        return foundMember.getPassword();
+    }
+    
+    public String getESPassword(String username) {
+        ExamSetter foundMember = eSList.get(username);
+        if (foundMember == null) {
+            return null;
+        }
         return foundMember.getPassword();
     }
 }
